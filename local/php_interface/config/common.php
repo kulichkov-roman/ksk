04482ddd726f5f0709a9aca6aee4822a872ad600
Kulichkov Roman <roman@kulichkov.pro>
2016-04-06 23:53:06 +0600

    Сделал слайдер на главной https://kulichkov.bitrix24.ru/workgroups/group/44/tasks/task/view/312/

--- local/php_interface/config/common.php
+++ local/php_interface/config/common.php
@@ -9,3 +9,19 @@
 		)
 	)
 );
+
+/**
+ * Перерезатор
+ */
+\Bitrix\Main\Loader::includeModule("itconstruct.resizer");
+
+/**
+ * Патерны для перерезатора
+ */
+itc\Resizer::addPreset('sliderMain', array(
+		'mode' => 'crop',
+		'width' => '1920',
+		'height' => '620',
+		'type' => 'jpg'
+	)
+);
