6d3bb781c3408b0340ebc56ccfbba80908b69dc0
Kulichkov Roman <roman@kulichkov.pro>
2016-04-06 22:53:23 +0600

    #migrate создал тип ИБ и ИБ "Слайдер" https://kulichkov.bitrix24.ru/workgroups/group/44/tasks/task/view/312/

--- local/php_interface/config/common.php
+++ local/php_interface/config/common.php
@@ -5,24 +5,7 @@
 \Quetzal\Environment\EnvironmentManager::getInstance()->addConfig(
 	new \Quetzal\Environment\Configuration\CommonConfiguration(
 		array(
-			/**
-			 * @todo нужно выполнить миграцию 002_create_hero_slider_iblock.php
-			 * и записать сюда полученный id
-			 */
-			// id ИБ "Слайдер"
-			'sliderHeroIBlockId' => 6,
-			/**
-			 * @todo нужно выполнить миграцию 004_create_feedback_iblock.php
-			 */
-			// id ИБ "Обратная связь"
-			'feedbackIBlockId' => 7,
-			'feedbackEmailPropId' => 8,
-			'feedbackPhonePropId' => 9,
-			/**
-			 * @todo нужно выполнить миграцию 006_create_contacts_slider_iblock.php
-			 */
-			// id ИБ "Контакты"
-			'contactsSliderIBlockId' => 8,
+			'sliderMainIBlockId' => 1,
 		)
 	)
 );
