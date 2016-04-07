1d481233238c5a17305eceb4c1cf00cddfa1fbe4
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2016-04-07 21:17:55 +0300

    added html source

---
+++ markup/gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var config = require('./config.json');
+
+var jadePath = null;
+
+gulp.task('default', function() {
+    gulp.start('buildScss');
+    gulp.start('buildJade');
+    watch('./src/scss/*.scss', function () {
+    	setTimeout(function() {
+            gulp.start('buildScss');
+        }, 100)
+    });
+
+    watch('./src/**/*.jade', function (file) {
+        if (file.path.match(/includes\\|includes\//)) {
+            jadePath = null;
+        } else {
+            jadePath = file.path;
+        }
+
+        setTimeout(function() {
+            gulp.start('buildJade');
+        }, 100);
+    });
+});
+
+
+gulp.task('buildScss', function() {
+    return gulp.src('./src/scss/' + config.cssFilename)
+    	// s.pipe(sourcemaps.init())
+    	.pipe(sass({
+            errLogToConsole: true
+        }))
+		// s.pipe(sourcemaps.write('./'))
+    	.pipe(gulp.dest('./../local/templates/.default/css/'));
+});
+
+
+gulp.task('buildJade', function() {
+    return gulp.src(jadePath || './src/*.jade')
+	    .pipe(jade({
+	    	locals: {},
+	    	pretty: '    '
+	    }))
+	    .pipe(gulp.dest('./../local/templates/.default/'));
+});
