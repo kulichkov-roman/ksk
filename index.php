b2285cb822d0c96252b76f62b6635c2fe25b87f3
Roman Kulichkov <roman@kulichkov.pro>
2016-06-06 18:15:40 +0600

    Убрал новости с главной во включаемую область

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle('Главная');
-?> 
-
+?>
+<?
+$APPLICATION->IncludeComponent('bitrix:main.include', '',
+    Array(
+        'AREA_FILE_SHOW' => 'file',
+        'PATH' => '/local/include/page_templates/pg_news_main.php',
+        'EDIT_TEMPLATE' => ''
+    ),
+    false,
+    Array('HIDE_ICONS' => 'Y')
+);
+$APPLICATION->IncludeComponent('bitrix:main.include', '',
+    Array(
+        'AREA_FILE_SHOW' => 'file',
+        'PATH' => '/local/include/site_templates/hd_rewards_main.php',
+        'EDIT_TEMPLATE' => ''
+    ),
+    false,
+    Array('HIDE_ICONS' => 'Y')
+);
+?>
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
