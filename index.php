1d34e0df97233f056b594916b6374209bf747c5b
Kulichkov Roman <roman@kulichkov.pro>
2016-06-07 01:09:35 +0600

    Вынес блок с отзывами в шаблон сайта

--- index.php
+++ index.php
@@ -12,15 +12,6 @@ $APPLICATION->IncludeComponent('bitrix:main.include', '',
     false,
     Array('HIDE_ICONS' => 'Y')
 );
-$APPLICATION->IncludeComponent('bitrix:main.include', '',
-    Array(
-        'AREA_FILE_SHOW' => 'file',
-        'PATH' => '/local/include/site_templates/hd_rewards_main.php',
-        'EDIT_TEMPLATE' => ''
-    ),
-    false,
-    Array('HIDE_ICONS' => 'Y')
-);
 ?>
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
